44feb5883d4f0aabfc74ca2590fbef7fe6175882
Viet Hoang <hello@vietamins.com>
2020-07-08 14:09:31 -0700

    Add bottom fade

--- src/components/plate-calculator/column-layout/column-layout.js
+++ src/components/plate-calculator/column-layout/column-layout.js
@@ -5,8 +5,8 @@ const ColumnLayout = ({
   children,
 }) => {
   return (
-    <div className="flex flex-1 h-screen">
-      <div className="flex-1 px-8 pt-6 pb-8 overflow-y-scroll">
+    <div className="flex flex-1 h-screen fade-bottom--white">
+      <div className="flex-1 px-8 pt-6 pb-8 overflow-y-scroll scrolling-touch">
         {children}
       </div>
     </div>
