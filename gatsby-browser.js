cbe25ddd39a69039bf06c0384149241b318fe5ac
Viet Hoang <hello@vietamins.com>
2020-07-07 22:04:56 -0700

    Ass SCSS support

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,4 +6,4 @@
 
 // You can delete this file if you're not using it
 
-import "./src/css/index.css"
+import "./src/css/index.scss"
--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        postCssPlugins: [
+          require("tailwindcss"),
+          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
